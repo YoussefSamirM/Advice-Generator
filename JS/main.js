6125843ea436a0c275b83fffff9ef51debb70b39
Youssef <youssef1110samir@gmail.com>
2024-10-18 23:42:18 +0300

    Create new files

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+let quote = document.querySelector(".advice h1");
+let button = document.querySelector("button");
+let i = document.querySelector(".advice button i");
+
+async function getAdvice() {
+  let respone = await fetch("/Json/advice_list.json");
+  let data = await respone.json();
+  setTimeout(() => {
+    quote.innerHTML = data[Math.floor(Math.random() * data.length)].advice;
+  }, 500);
+}
+
+button.onclick = function () {
+  getAdvice();
+  rotate();
+};
+
+function rotate() {
+  setTimeout(() => {
+    i.classList.toggle("rotate");
+  }, 500);
+}
